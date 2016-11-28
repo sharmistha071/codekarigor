2f98f1f0740621c508325df8bc812d2e57d1d94a
shormy <shormy.roy@gmail.com>
2016-11-28 16:25:44 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', function (req, res) {
+    res.send('hello world');
+});*/
+
+
+app.listen(3000, function () {
+    console.log('server is running');
+});
